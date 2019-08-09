d763a8c5231fa6e1a4984b19f4ee9187997bd593
Emilio <thecraftedgem@tuta.io>
2019-08-09 14:43:18 -0600

    broke get request

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,24 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use Phoenix.LiveView
+  alias LiveViewTodos.Todos
+  alias LiveViewTodosWeb.TodoView
+
+  def mount(_session, socket) do
+    {:ok, fetch(socket)}
+  end
+
+  def render(assigns) do
+    TodoView.render("todos.html", assigns)
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todo: Todos.list_todos())
+  end
+
+end
