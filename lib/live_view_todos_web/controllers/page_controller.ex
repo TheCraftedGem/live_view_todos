6fd05c3fa9a1de21e2fb66a327b90aa5057788a3
Emilio <thecraftedgem@tuta.io>
2019-08-12 21:35:52 -0600

    wip

--- lib/live_view_todos_web/controllers/page_controller.ex
+++ lib/live_view_todos_web/controllers/page_controller.ex
@@ -3,6 +3,6 @@ defmodule LiveViewTodosWeb.PageController do
   alias Phoenix.LiveView
 
   def index(conn, _params) do
-    LiveView.Controller.live_render(conn, LiveViewTodosWeb.TodoLive, session: %{})
+    render(conn, "todos.html")
   end
 end
